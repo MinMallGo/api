d98d0edc6833eaa871f52f9f3be2de632aaccc6f
ucdo <phpsarc@gmail.com>
2025-06-16 09:58:38 +0800

    feat
    api 接口添加consul健康检查接口

--- user_api/initialize/router.go
+++ user_api/initialize/router.go
@@ -7,6 +7,11 @@ import (
 
 // InitRouter 初始化路由
 func InitRouter(g *gin.Engine) *gin.Engine {
+	// 添加一个健康检测的接口
+	g.GET("health", func(context *gin.Context) {
+		context.JSON(200, "")
+		return
+	})
 	group := g.Group("/v1")
 	// 注册用户路由
 	router.RegisterUserRouter(group)
