05112bebc3183155da440ce43a43195bc8d2c983
ucdo <phpsarc@gmail.com>
2025-06-17 10:36:00 +0800

    feat
    使用nacos配置中心，代替配置文件

--- user_api/config/config.go
+++ user_api/config/config.go
@@ -1,31 +1,41 @@
 package config
 
 type Config struct {
-	Name       string    `mapstructure:"name"`
-	Port       int       `mapstructure:"port"`
-	UserServer UserSrv   `mapstructure:"user_srv"`
-	Jwt        JwtSrv    `mapstructure:"jwt"`
-	Redis      RedisCnf  `mapstructure:"redis"`
-	Consul     ConsulCnf `mapstructure:"consul"`
+	Name       string    `mapstructure:"name" json:"name"`
+	Port       int       `mapstructure:"port" json:"port"`
+	UserServer UserSrv   `mapstructure:"user_srv" json:"user_srv"`
+	Jwt        JwtSrv    `mapstructure:"jwt" json:"jwt"`
+	Redis      RedisCnf  `mapstructure:"redis" json:"redis"`
+	Consul     ConsulCnf `mapstructure:"consul" json:"consul"`
 }
 
 type UserSrv struct {
-	Host string `mapstructure:"host"`
-	Port int    `mapstructure:"port"`
-	Name string `mapstructure:"name"`
+	Host string `mapstructure:"host" json:"host"`
+	Port int    `mapstructure:"port" json:"port"`
+	Name string `mapstructure:"name" json:"name"`
 }
 
 type JwtSrv struct {
-	Key string `mapstructure:"key"`
+	Key string `mapstructure:"key" json:"key"`
 }
 
 type RedisCnf struct {
-	Host     string `mapstructure:"host"`
-	Port     int    `mapstructure:"port"`
-	Password string `mapstructure:"password"`
+	Host     string `mapstructure:"host" json:"host"`
+	Port     int    `mapstructure:"port" json:"port"`
+	Password string `mapstructure:"password" json:"password"`
 }
 
 type ConsulCnf struct {
-	Host string `mapstructure:"host"`
-	Port int    `mapstructure:"port"`
+	Host string `mapstructure:"host" json:"host"`
+	Port int    `mapstructure:"port" json:"port"`
+}
+
+type NacosCnf struct {
+	Host      string `json:"host"`
+	Port      uint64 `json:"port"`
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+	Namespace string `json:"namespace"`
+	DataID    string `json:"data_id"`
+	Group     string `json:"group"`
 }
--- user_api/initialize/config.go
+++ user_api/initialize/config.go
@@ -1,13 +1,21 @@
 package initialize
 
 import (
-	"api/user_api/global"
+	"encoding/json"
 	"fmt"
-	"github.com/fsnotify/fsnotify"
-	"github.com/spf13/viper"
-	"go.uber.org/zap"
 	"log"
 	"os"
+	"path/filepath"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/nacos-group/nacos-sdk-go/v2/clients"
+	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/v2/vo"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+
+	"api/user_api/config"
+	"api/user_api/global"
 )
 
 /*
@@ -17,12 +25,96 @@ import (
 	4. 将读取到的配置文件赋予全局config变量
 */
 
+var (
+	nacos = &config.NacosCnf{}
+)
+
 func GetEnv(env string) bool {
 	viper.AutomaticEnv()
 	return viper.GetBool(env)
 }
 
+// 修改成从本地读取nacos的配置文件
+// 然后从nacos获取配置，再然后再解析nacos的配置文件到本地
+
 func InitConfig() {
+	curPath, _ := os.Getwd()
+	configName := "config.json"
+	debug := GetEnv("GoProjectDebug")
+	if debug {
+		configName = "config_debug.json"
+	}
+
+	file, err := os.ReadFile(filepath.Join(curPath, configName))
+	if err != nil {
+		zap.L().Fatal("[InitConfig] 读取nacos配置文件失败:", zap.Error(err))
+	}
+
+	err = json.Unmarshal(file, nacos)
+	if err != nil {
+		zap.L().Fatal("[InitConfig] 解析nacos配置文件失败:", zap.Error(err))
+	}
+
+	// 读取nacos的配置文件之后，打印以下看看
+	fmt.Printf("nacos配置文件：%#v\n", nacos)
+
+	// 连接nacos获取配置
+	cnf, err := getConfig()
+	if err != nil {
+		zap.L().Fatal("[InitConfig].[getConfig] with error:", zap.Error(err))
+	}
+
+	err = json.Unmarshal([]byte(cnf), &global.Cfg)
+	if err != nil {
+		zap.L().Fatal("[InitConfig].[json.Unmarshal] with error:", zap.Error(err))
+	}
+	zap.L().Info("[InitConfig] <UNK>", zap.Any("cnf", cnf))
+}
+
+func getConfig() (string, error) {
+	clientConfig := constant.ClientConfig{
+		NamespaceId:         nacos.Namespace, // 如果需要支持多namespace，我们可以创建多个client,它们有不同的NamespaceId。当namespace是public时，此处填空字符串。
+		TimeoutMs:           5000,
+		NotLoadCacheAtStart: true,
+		LogDir:              "tmp/nacos/log",
+		CacheDir:            "tmp/nacos/cache",
+		LogLevel:            "debug",
+		Username:            nacos.Username, // Nacos服务端的API鉴权Username
+		Password:            nacos.Password, // Nacos服务端的API鉴权Password
+	}
+
+	serverConfigs := []constant.ServerConfig{
+		{
+			IpAddr:      nacos.Host,
+			ContextPath: "/nacos",
+			Port:        nacos.Port,
+			Scheme:      "http",
+		},
+	}
+
+	configClient, err := clients.NewConfigClient(
+		vo.NacosClientParam{
+			ClientConfig:  &clientConfig,
+			ServerConfigs: serverConfigs,
+		},
+	)
+
+	if err != nil {
+		return "", err
+	}
+
+	content, err := configClient.GetConfig(vo.ConfigParam{
+		DataId: nacos.DataID,
+		Group:  nacos.Group,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return content, nil
+}
+
+func InitConfig2() {
 	curPath, _ := os.Getwd()
 	configName := "config.yaml"
 	debug := GetEnv("GoProjectDebug")
